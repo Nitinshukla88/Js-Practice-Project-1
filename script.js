75d2ba5285c51cd73e21b634bd6a53c157645333
Nitin shukla <nitin.shukla12004@gmail.com>
2024-06-19 19:37:52 +0530

    wrote some js for coloring of box

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 outerdiv = document.querySelector('.con')
 
-outerdiv.addEventListener('mouseover',function(){
-    outerdiv.style.backgroundColor = "red"
+outerdiv.addEventListener('mousemove',function(event){
+    var val = outerdiv.getBoundingClientRect()
+    if((event.clientX-val.left)<val.width/2){
+        outerdiv.style.backgroundColor = "#"+(255).toString(16)+(20).toString(16)+(20).toString(16)
+    }else{
+        outerdiv.style.backgroundColor = "green"
+    }
 })
 
-outerdiv.addEventListener('mouseout',function(){
+outerdiv.addEventListener("mouseout",function(){
     outerdiv.style.backgroundColor = "white"
 })
 
